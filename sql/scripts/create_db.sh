a81fdf0e1c3e8eb4deeb73744c3cc5f22ff46716
Yuan Yao <yyao009@well.cs.ucr.edu>
2016-03-14 06:51:34 -0700

    Fixed

--- sql/scripts/create_db.sh
+++ sql/scripts/create_db.sh
@@ -3,3 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 psql -p $PGPORT $DB_NAME < $DIR/../src/create_tables.sql
 psql -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
 psql -p $PGPORT $DB_NAME < $DIR/../src/load_data.sql
+psql -p $PGPORT $DB_NAME < $DIR/../src/create_indexes.sql
+psql -p $PGPORT $DB_NAME < $DIR/../src/triggers.sql
--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,32 +1,32 @@
 COPY USER_LIST
-FROM '/home/ubuntu/workspace/messenger/data/usr_list.csv'
+FROM '/home/csgrads/yyao009/messenger/data/usr_list.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE user_list_list_id_seq RESTART 55906;
 
 COPY USR
-FROM '/home/ubuntu/workspace/messenger/data/usr.csv'
+FROM '/home/csgrads/yyao009/messenger/data/usr.csv'
 WITH DELIMITER ';';
 
 COPY USER_LIST_CONTAINS
-FROM '/home/ubuntu/workspace/messenger/data/usr_list_contains.csv'
+FROM '/home/csgrads/yyao009/messenger/data/usr_list_contains.csv'
 WITH DELIMITER ';';
 
 COPY CHAT
-FROM '/home/ubuntu/workspace/messenger/data/chat.csv'
+FROM '/home/csgrads/yyao009/messenger/data/chat.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE chat_chat_id_seq RESTART 5001;
 
 COPY CHAT_LIST
-FROM '/home/ubuntu/workspace/messenger/data/chat_list.csv'
+FROM '/home/csgrads/yyao009/messenger/data/chat_list.csv'
 WITH DELIMITER ';';
 
 COPY MESSAGE
-	(msg_id, 
-	msg_text, 
-	msg_timestamp, 
+	(msg_id,
+	msg_text,
+	msg_timestamp,
 	sender_login,
 	chat_id)
-FROM '/home/ubuntu/workspace/messenger/data/message.csv'
+FROM '/home/csgrads/yyao009/messenger/data/message.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE message_msg_id_seq RESTART 50000;
 
